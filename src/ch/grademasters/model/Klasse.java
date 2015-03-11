6a961ceeda2c9a905cbf70c2a6dac0dc44ffd31c
zpengc-bbc <chirametphong.penglerd@bbcag.ch>
2015-03-11 11:27:36 +0100

    comment added xD
    Signed-off-by: zmartl-bbc <luca.marti@bbcag.ch>

--- src/ch/grademasters/model/Klasse.java
+++ src/ch/grademasters/model/Klasse.java
@@ -1,5 +1,9 @@
 package ch.grademasters.model;
-
+/**
+ * 
+ * @author Chiramet Phong Penglerd, ICT Berufsbildungscenter AG, chirametphong.penglerd@bbcag.ch
+ *
+ */
 public class Klasse {
 	
 	//Instanzvariablen
