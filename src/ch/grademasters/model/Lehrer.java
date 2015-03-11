b22ac22dd4c3a88ff9114b00c64c5094c8bc8df1
zperee-bbc <elia.perenzin@bbcag.ch>
2015-03-11 10:16:00 +0100

    Add Setter and Getter Test.java and Lehrer.java

--- src/ch/grademasters/model/Lehrer.java
+++ src/ch/grademasters/model/Lehrer.java
@@ -6,4 +6,45 @@ public class Lehrer {
 	private String name;
 	private String vorname;
 	private String email;
+	
+	//Getter und Setter
+	/**
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return vorname
+	 */
+	public String getVorname() {
+		return vorname;
+	}
+	/**
+	 * @param vorname
+	 */
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	
+	/**
+	 * @return email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 }
--- src/ch/grademasters/model/Test.java
+++ src/ch/grademasters/model/Test.java
@@ -5,4 +5,33 @@ public class Test {
 	//Instanzvariablen
 	private float note;
 	private float gewichtung;
+	
+	//Getter und Setter
+	/**
+	 * @return note
+	 */
+	public float getNote() {
+		return note;
+	}
+	/**
+	 * @param note
+	 */
+	public void setNote(float note) {
+		this.note = note;
+	}
+	
+	/**
+	 * @return gewichtung
+	 */
+	public float getGewichtung() {
+		return gewichtung;
+	}
+	/**
+	 * @param gewichtung
+	 */
+	public void setGewichtung(float gewichtung) {
+		this.gewichtung = gewichtung;
+	}
+	
+	
 }
