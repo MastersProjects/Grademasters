aff617a3275ce17a4cde56e0559f7cea55c83f22
zpengc-bbc <chirametphong.penglerd@bbcag.ch>
2015-03-11 11:34:06 +0100

    Signed-off-by: zpengc-bbc <chirametphong.penglerd@bbcag.ch>

--- src/ch/grademasters/model/Pruefung.java
+++ src/ch/grademasters/model/Pruefung.java
@@ -4,7 +4,7 @@ package ch.grademasters.model;
  * @author Chiramet Phong Penglerd, ICT Berufsbildungscenter AG, chirametphong.penglerd@bbcag.ch
  *
  */
-public class Test {
+public class Pruefung {
 	
 	//Instanzvariablen
 	private float note;
