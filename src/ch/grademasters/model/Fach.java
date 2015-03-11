de7094e154fd29dbdfc368736b564f0b0ea80167
zpengc-bbc <chirametphong.penglerd@bbcag.ch>
2015-03-11 13:26:41 +0100

    Signed-off-by: zpengc-bbc <chirametphong.penglerd@bbcag.ch>

--- src/ch/grademasters/model/Fach.java
+++ src/ch/grademasters/model/Fach.java
@@ -10,4 +10,44 @@ public class Fach {
 	
 	//Instanzvariablen
 	private String fach;
+	private Pruefung pruefung = new Pruefung();
+	
+
+	
+	//Getter und Setter
+	/**
+	* @return fach
+	*/
+	public String getFach() {
+		return fach;
+	}
+
+	/**
+	 * @param fach
+	 */
+	public void setFach(String fach) {
+		this.fach = fach;
+	}
+
+	/**
+	* @return pruefung
+	*/
+	public Pruefung getPruefung() {
+		return pruefung;
+	}
+	/**
+	 * @param pruefung
+	 */
+	public void setPruefung(Pruefung pruefung) {
+		this.pruefung = pruefung;
+	}
+	
+	//Methode
+	
+	/**
+	* @return ZeugnisNote
+	*/
+	public float berechneZeugnisNote(float note) {
+		return this.getPruefung().getNote();
+	}
 }
