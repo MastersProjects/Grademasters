cb7810f35e121a3a25203bb7112e7a9072ccdb1d
zpengc-bbc <chirametphong.penglerd@bbcag.ch>
2015-03-12 15:31:24 +0100

    Update :tada:

--- src/ch/grademasters/view/GradeMastersView.java
+++ src/ch/grademasters/view/GradeMastersView.java
@@ -21,8 +21,10 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JToolBar;
 
+import ch.grademasters.model.Fach;
 import ch.grademasters.model.Klasse;
 import ch.grademasters.model.KlassenLehrer;
+import ch.grademasters.model.Pruefung;
 
 
 public class GradeMastersView extends JFrame {
@@ -93,6 +95,25 @@ public class GradeMastersView extends JFrame {
 		//Lehrer erstellen
 		KlassenLehrer lehrer = new KlassenLehrer("Guenther", "Schneider", "Guenter.Schneider@Access.tbz");
 		Klasse klasse = new Klasse(lehrer, "AP14a", "TBZ", 2);
+		//Erstes Beispiel 
+				Fach mathe = new Fach("Mathe");
+				
+				klasse.getZeugnis().addFach(mathe);
+				Pruefung pruefung1 = new Pruefung("Gleichungen", 6f, 1f);
+				Pruefung pruefung2 = new Pruefung("Eins mal Eins", 5.76f, 0.5f);		
+				mathe.addPruefung(pruefung1);
+				mathe.addPruefung(pruefung2);
+				
+				//Zweites Beispiel 
+				Fach deutsch = new Fach("Deutsch");
+				
+				klasse.getZeugnis().addFach(deutsch);
+				Pruefung pruefung3 = new Pruefung("Gramatik", 4.25f, 1f);
+				Pruefung pruefung4 = new Pruefung("Nomen", 5.25f, 0.5f);
+				Pruefung pruefung5 = new Pruefung("Verben", 4.8f, 0.5f);		
+				deutsch.addPruefung(pruefung3);
+				deutsch.addPruefung(pruefung4);
+				deutsch.addPruefung(pruefung5);
 		
 		// Text Fach
 				JLabel fachLabel = new JLabel("Bitte wähle dein Fach aus: ");
@@ -104,7 +125,10 @@ public class GradeMastersView extends JFrame {
 		klasseGrid.add(klasseLabel);
 		
 		// Scrollbar 1
-		Object schulKlasse[] = { klasse };
+		String schulKlasse[] = { "Klasse: " + klasse.getKlassenname() + "  |  " 
+								+ "Semester: " + klasse.getSemester() + "  |  " 
+								+ "Schule: " + klasse.getSchule() 
+		};
 
 		JList<?> list = new JList<Object>(schulKlasse);
 		scrollpaneKlasse = new JScrollPane(list);
@@ -115,8 +139,9 @@ public class GradeMastersView extends JFrame {
 		klasseGrid.add(fachLabel);
 				
 		// Scrollbar 2
-		String faecher[] = { "Mathe", "Deutsch", "Französisch",
-				"Englisch" };
+		String faecher[] = {};
+		
+		
 
 		JList<?> list2 = new JList<Object>(faecher);
 		scrollpaneFach = new JScrollPane(list2);
