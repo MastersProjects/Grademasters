400974f89a6a377f278a8871e410600d04222bd8
zperee-bbc <elia.perenzin@bbcag.ch>
2015-03-19 08:44:08 +0100

    Datum spinner fuer note hinzufuegen in der Klasse datumTest
    Signed-off-by: zperee-bbc <elia.perenzin@bbcag.ch>

--- src/test/labor/datumTest.java
+++ src/test/labor/datumTest.java
@@ -1,15 +1,33 @@
 package test.labor;
 
-import javax.swing.JFrame;
+import java.awt.FlowLayout;
+import java.util.Calendar;
+import java.util.Date;
 
-import ch.grademasters.exception.SQLError;
- 
-public class datumTest extends JFrame
-{	
-	public static void main(String[] args)
-	{
-		new SQLError();
- 
-	}
-	
-	}
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerDateModel;
+
+public class datumTest extends JFrame {
+
+  public datumTest () {
+    setSize(200, 100);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    setLayout(new FlowLayout(FlowLayout.LEFT, 4, 4));
+
+    add(new JLabel("Datum: "));
+    Date today = new Date();
+    JSpinner datum = new JSpinner(new SpinnerDateModel(today, null, null, Calendar.MONTH));
+    JSpinner.DateEditor datumformatiert = new JSpinner.DateEditor(datum, "dd/MM/yyyy");
+    datum.setEditor(datumformatiert);
+    add(datum);
+
+    setVisible(true);
+  }
+
+  public static void main(String args[]) {
+    new datumTest();
+  }
+}
