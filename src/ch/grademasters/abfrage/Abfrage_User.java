e2f4a5c396b53d700b1f447a81552d68e5130c24
zmartl <luca.marti@bbcag.ch>
2015-03-20 14:04:56 +0100

    Signed-off-by: zmartl <luca.marti@bbcag.ch>

--- src/ch/grademasters/abfrage/Abfrage_User.java
+++ src/ch/grademasters/abfrage/Abfrage_User.java
@@ -21,18 +21,25 @@ import ch.grademasters.view.GradeMastersView;
 public class Abfrage_User extends JDialog implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
+	//Label fuer GUI
 	Label LabelUsername = new Label("Username");
 	Label LabelPasswort = new Label("Passwort");
-	static JTextField TextUsername = new JTextField(20); // Laenge angeben
-	static JPasswordField FieldPasswort = new JPasswordField(20); // Laenge
-	static Button ButtonLogin = new Button("Login");
 	Label LabelRegistration = new Label(
 			"Noch keinen Account? Hier Registrieren");
+	
+	//TextFields fuer GUI
+	static JTextField TextUsername = new JTextField(20); 
+	static JPasswordField FieldPasswort = new JPasswordField(20);
+	
+	//Button fuer GUI
+	static Button ButtonLogin = new Button("Login");
 	static Button ButtonRegistration = new Button("Registrieren");
 
 	public Abfrage_User() {
-
-		setLayout(new FlowLayout()); // Layout definieren
+		//Layout definieren
+		setLayout(new FlowLayout());
+		
+		//Hinzufuegen zum GUI
 		add(LabelUsername);
 		add(TextUsername);
 		add(LabelPasswort);
@@ -42,13 +49,18 @@ public class Abfrage_User extends JDialog implements ActionListener {
 		add(ButtonRegistration);
 		ButtonLogin.addActionListener(this);
 		ButtonRegistration.addActionListener(this);
+		
+		//Eigenschaften des GUI
 		setSize(275, 250);
 		setVisible(true);
 		setResizable(false);
+		
+		//Add WindowListener
 		addWindowListener(new WindowListener());
 
 	}
 
+	//WindowListener Event
 	class WindowListener extends WindowAdapter {
 		public void windowClosing(WindowEvent e) {
 			System.exit(0);
@@ -56,24 +68,38 @@ public class Abfrage_User extends JDialog implements ActionListener {
 	}
 
 	public static void main(String[] args) {
-		Abfrage_User gui = new Abfrage_User(); // Abfrage_User aufrufen
+		
+		// Abfrage_User aufrufen
+		Abfrage_User gui = new Abfrage_User();
 		gui.setLocationRelativeTo(null);
+		
 		// Action Listener erstellen
 		ButtonLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//Final Strings fuer Username und Passwort
 				final String username = TextUsername.getText();
 				final String passwort = new String(FieldPasswort.getPassword());
 				try {
+					//Neuer User anlegen
 					User currentUser = new User();
+					
+					//Username und Passwort setzen
 					currentUser.setUsername(username);
 					currentUser.setPasswort(passwort);
+					
+					//Wenn login == true, dann oeffnen des Programmes
 					if (GMController.getInstance().login(currentUser) == true) {
+						//Login GUI nicht sichbar machen
 						gui.setVisible(false);
+						//Neue GradeMastersView()
 						GradeMastersView gradeMastersView = new GradeMastersView();
+						//GradeMastersView in die Mitte des Bildschirmes stellen
 						gradeMastersView.setLocationRelativeTo(null);
 					}
 				}
+				//Exception auffange
 				catch (Exception e1) {
+					//Neuer SQLError aufrufen
 					SQLError qulError = new SQLError();
 					qulError.setLocationRelativeTo(null);
 				}
@@ -82,9 +108,12 @@ public class Abfrage_User extends JDialog implements ActionListener {
 			}
 		});
 
+		//ButtonRegistration - ActionListener
 		ButtonRegistration.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//Neues Fenster Registr
 				Registr registr = new Registr();
+				//Fenster in die Mitte setzen
 				registr.setLocationRelativeTo(null);
 			}
 		});
