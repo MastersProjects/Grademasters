c0b1b14dda7d2add6eb4a91fc47fbff64f07b92f
zmartl <luca.marti@bbcag.ch>
2015-03-20 14:17:29 +0100

    UserJDBCDao.java kommentiert
    Signed-off-by: zmartl <luca.marti@bbcag.ch>

--- src/ch/grademasters/dao/UserJDBCDao.java
+++ src/ch/grademasters/dao/UserJDBCDao.java
@@ -14,34 +14,50 @@ import ch.grademasters.model.User;
  */
 
 public class UserJDBCDao extends Database implements UserDao {
+	//Connection con auf null setzen
 	private Connection con = null;
 
 	@Override
 	public void insertUser(User user) throws SQLException {
+		//SQL Query
 		String sql = "INSERT INTO USER (Username, Passwort) VALUES (?, ?)";
+		//Connection
 		con = getCon();
 		ps = con.prepareStatement(sql);
 		ps.setString(1, user.getUsername());
 		ps.setString(2, user.getPasswort());
+		//Query ausfuehren
 		ps.executeUpdate();
+		//Connection schliessen (methode)
 		closeCon();
 	}
 
 	@Override
 	public List<User> findAllUsers() throws SQLException {
+		//SQL Query
 		String sql = "SELECT * FROM USER";
+		//User p ArrayList
 		List<User> p = new ArrayList<User>();
+		//Connection con
 		con = getCon();
 		ps = con.prepareStatement(sql);
+		//Query ausfuehren
 		rs = ps.executeQuery();
 
+		//Macht es solange Datensaetze vorhande
 		while (rs.next()) {
+			//Neuer User anlegen
 			User user = new User();
+			//Usernamen setzen
 			user.setUsername(rs.getString("Username"));
+			//Passwort setzen
 			user.setPasswort(rs.getString("Passwort"));
+			//User zu ArrayList hinzufuegen
 			p.add(user);
 		}
+		//Connection schliessen
 		closeCon();
+		//return p
 		return p;
 
 	}
