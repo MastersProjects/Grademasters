1b3b68412b035fc6ff90ba235f9afa03da2811d7
zperee-bbc <elia.perenzin@bbcag.ch>
2015-03-20 09:44:12 +0100

    Signed-off-by: zperee-bbc <elia.perenzin@bbcag.ch>

--- src/ch/grademasters/dao/LehrerJDBCDao.java
+++ src/ch/grademasters/dao/LehrerJDBCDao.java
@@ -1,30 +1,33 @@
 package ch.grademasters.dao;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import ch.grademasters.model.Klasse;
 
-public class LehrerJDBCDao extends Database implements
-LehrerDao {
+public class LehrerJDBCDao extends Database implements LehrerDao {
 
 	private Connection con = null;
-	
-	
+
 	@Override
 	public int addLehrer(Klasse klasse) throws SQLException {
 		String sql = "INSERT INTO KLASSENLEHRER (Name, Vorname, Email) VALUES (?, ?, ?)";
 		con = getCon();
-		ps = con.prepareStatement(sql);
+		ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 		ps.setString(1, klasse.getKlassenLehrer().getName());
 		ps.setString(2, klasse.getKlassenLehrer().getVorname());
 		ps.setString(3, klasse.getKlassenLehrer().getEmail());
-		int i = ps.executeUpdate();
+		ps.executeUpdate();
+		ResultSet rs = ps.getGeneratedKeys();
+		int i = 0;
+		while (rs.next()) {
+			i = rs.getInt(1);
+			break;
+		}
 		closeCon();
 		return i;
-		
 	}
-
-	
 
 }
