5b75e3cbf27fdbbeeac7dc7b569ce032975f391b
zmartl <luca.marti@bbcag.ch>
2015-03-25 08:43:25 +0100

    PDF Start
    Signed-off-by: zmartl <luca.marti@bbcag.ch>

---
+++ src/ch/grademasters/pdf/ZeugnisPdf.java
@@ -0,0 +1,26 @@
+package ch.grademasters.pdf;
+
+import java.io.FileOutputStream;
+import java.util.Date;
+
+import com.itextpdf.text.Anchor;
+import com.itextpdf.text.BadElementException;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chapter;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.List;
+import com.itextpdf.text.ListItem;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Section;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class ZeugnisPdf {
+	public static String file = "T:\_Team\zmartl\Module\08 - Anwendungsentwicklung\Grademasters";
+
+}
---
+++ src/ch/grademasters/pdf/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Luca Marti, ICT Berufsbildungscenter AG, luca.marti@bbcag.ch
+ * @value package-info.java
+ * @year 2015
+ */
+package ch.grademasters.pdf;
