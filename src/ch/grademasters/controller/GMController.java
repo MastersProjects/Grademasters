b1d618153da28817dba4b1bc500d8e0a5353eda4
zperee-bbc <elia.perenzin@bbcag.ch>
2015-03-26 10:20:57 +0100

    :point_right: Login Error fixed :point_left: :raised_hands: :smirk:
    Signed-off-by: zperee-bbc <elia.perenzin@bbcag.ch>

--- src/ch/grademasters/controller/GMController.java
+++ src/ch/grademasters/controller/GMController.java
@@ -35,9 +35,8 @@ import ch.grademasters.util.EncryptUtils;
 
 /**
  * @description
- * @author Luca Marti, Chiramet Phong Penglerd, Elia Perenzin 
- * GMController.java
- * Copyright Berufsbildungscenter GradeMasters 2015
+ * @author Luca Marti, Chiramet Phong Penglerd, Elia Perenzin GMController.java
+ *         Copyright Berufsbildungscenter GradeMasters 2015
  */
 
 public class GMController {
@@ -48,7 +47,7 @@ public class GMController {
 	private static final FachDao FACH_DAO = new FachJDBCDao();
 	private static final NoteDao NOTE_DAO = new NoteJDBCDao();
 	private static final ZeugnisDao ZEUGNIS_DAO = new ZeugnisJDBCDao();
-	
+
 	/**
 	 * Konstruktor der Klasse GMCController nur Privat
 	 */
@@ -58,9 +57,10 @@ public class GMController {
 	public static GMController getInstance() {
 		return GMController.instance;
 	}
-	
+
 	/**
 	 * Lehrer hizufuegen in die DB
+	 * 
 	 * @param klasse
 	 */
 	public void klasseSpeichern(Klasse klasse) {
@@ -76,20 +76,23 @@ public class GMController {
 
 	/**
 	 * Alle Klassen auslesen aus der DB fuer GUI
+	 * 
 	 * @return alle Klassen in Vector
 	 */
 	public Vector<?> getKlasse() {
 		Vector<?> klasse = null;
 		try {
 			klasse = KLASSE_DAO.getKlasse();
-		} catch (SQLException e) {
+		}
+		catch (SQLException e) {
 			new SQLError();
 		}
 		return klasse;
 	}
-	
+
 	/**
 	 * Alle Faecher auslesen aus DB fuer GUI
+	 * 
 	 * @param klasse_ID
 	 * @return Alle Faecher einer bestimmten Klasse in Vector
 	 */
@@ -97,30 +100,34 @@ public class GMController {
 		Vector<?> fach = null;
 		try {
 			fach = FACH_DAO.getFachById(klasse_ID);
-		} catch (SQLException e) {
+		}
+		catch (SQLException e) {
 			new SQLError();
 		}
-		return fach;	
+		return fach;
 	}
-	
+
 	/**
 	 * 
 	 */
-	public Fach getNotenByID(int fach_ID){
+	public Fach getNotenByID(int fach_ID) {
 		try {
 			return NOTE_DAO.getNotenByID(fach_ID);
-		} catch (SQLException e) {
+		}
+		catch (SQLException e) {
 			new SQLError();
 		}
-		return null;		
+		return null;
 	}
-	
+
 	/**
 	 * Ein neues Fach in die DB speichern
-	 * @param ID_Klasse, fachName
+	 * 
+	 * @param ID_Klasse
+	 *            , fachName
 	 */
-	public void fachSpeichern(int ID_Klasse, String fachName){
-		try{
+	public void fachSpeichern(int ID_Klasse, String fachName) {
+		try {
 			FACH_DAO.addFach(ID_Klasse, fachName);
 			new addInDb();
 		}
@@ -128,13 +135,16 @@ public class GMController {
 			new SQLError();
 		}
 	}
-	
+
 	/**
 	 * Eine neue Note in die Db eintragen
-	 * @param note, gewichtung, fach_ID, benennung
+	 * 
+	 * @param note
+	 *            , gewichtung, fach_ID, benennung
 	 */
-	public void noteSpeichern(float note, float gewichtung, int fach_ID, String benennung) {
-		try{
+	public void noteSpeichern(float note, float gewichtung, int fach_ID,
+			String benennung) {
+		try {
 			NOTE_DAO.addNote(note, gewichtung, fach_ID, benennung);
 			new addInDb();
 		}
@@ -142,26 +152,28 @@ public class GMController {
 			new SQLError();
 		}
 	}
-	
+
 	/**
 	 * Liest alle Noten aus der DB und erstellt die dazugehoerigen Facher
+	 * 
 	 * @return Faecher Objekte mit allen Noten
 	 */
-	public ArrayList<Fach> getNotenUndFach(){
-		try{
-			return NOTE_DAO.getNotenUndFach(); 
+	public ArrayList<Fach> getNotenUndFach() {
+		try {
+			return NOTE_DAO.getNotenUndFach();
 		}
-		catch (SQLException e){
+		catch (SQLException e) {
 			new SQLError();
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Holt alle Informationen aus DB um ein Zeugnis zuerstellen
+	 * 
 	 * @return
 	 */
-	public ArrayList<?> getZeugnis(){
+	public ArrayList<?> getZeugnis() {
 		try {
 			return ZEUGNIS_DAO.getZeugnis();
 		}
@@ -170,9 +182,10 @@ public class GMController {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Methode um neuen User in die DB einzutragen.
+	 * 
 	 * @param currentUser
 	 */
 	public void insert(User currentUser) {
@@ -214,8 +227,9 @@ public class GMController {
 
 	/**
 	 * Ueberprueft die eingaben des Users und preuft ob login korrekt
+	 * 
 	 * @param currentUser
-	 * @return  Boolean ob Login richtig oder falsch
+	 * @return Boolean ob Login richtig oder falsch
 	 */
 	public boolean login(User currentUser) {
 		List<User> dbUsers = null;
@@ -231,6 +245,7 @@ public class GMController {
 		String passwortLocal = currentUser.getPasswort();
 
 		boolean login = false;
+		boolean blank = false;
 		int i = 1;
 
 		for (User dbUser : dbUsers) {
@@ -247,16 +262,10 @@ public class GMController {
 						&& StringUtils.isNotBlank(passwortDb)
 						&& passwortLocal.equals(passwortDb)) {
 					login = true;
-					
 
 				}
 				else if (!passwortLocal.equals(passwortDb)) {
-					if (i < 2) {
-						LoginError loginError = new LoginError();
-						loginError.setLocationRelativeTo(null);
-						i = i + 1;
-						login = false;
-					}
+					login = false;
 				}
 				else if (StringUtils.isBlank(passwortLocal)) {
 					if (i < 2) {
@@ -274,19 +283,16 @@ public class GMController {
 					userError.setLocationRelativeTo(null);
 					i = i + 1;
 					login = false;
+					blank = true;
 				}
 				break;
 			}
-			if (!login) {
-				if (i < 2) {
-					LoginError loginError = new LoginError();
-					loginError.setLocationRelativeTo(null);
-					i = i + 1;
-					login = false;
-				}
-			}
+		}
+		if (login == false && blank == false) {
+			LoginError loginError = new LoginError();
+			loginError.setLocationRelativeTo(null);
 		}
 		return login;
 	}
-	
+
 }
