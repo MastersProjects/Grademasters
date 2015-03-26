601182303943877cc5c19951be3d9813f045b1bd
zpengc-bbc <chirametphong.penglerd@bbcag.ch>
2015-03-26 08:56:23 +0100

    Signed-off-by: zpengc-bbc <chirametphong.penglerd@bbcag.ch>

--- src/ch/grademasters/listener/StartCardFachListener.java
+++ src/ch/grademasters/listener/StartCardFachListener.java
@@ -7,6 +7,8 @@ import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
+import ch.grademasters.item.Item;
+
 public class StartCardFachListener implements ListSelectionListener {
 
 	
@@ -19,7 +21,8 @@ public class StartCardFachListener implements ListSelectionListener {
 	@Override
 	public void valueChanged(ListSelectionEvent e) {
 		ListSelectionModel lsm = (ListSelectionModel)e.getSource();
-		Item item = lsm
+		int item = lsm.getSelectionMode();
+		System.out.println(item);
 		
 		
 	}
