4024b55722d2e69de2e8851799d2ca8951ae2b11
zmartl <luca.marti@bbcag.ch>
2015-03-27 11:04:53 +0100

    Kommentieren :heavy_exclamation_mark: :rage:
    Signed-off-by: zmartl <luca.marti@bbcag.ch>

--- src/ch/grademasters/dao/ZeugnisJDBCDao.java
+++ src/ch/grademasters/dao/ZeugnisJDBCDao.java
@@ -1,6 +1,7 @@
 package ch.grademasters.dao;
 
 import java.sql.Connection;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -10,64 +11,110 @@ import ch.grademasters.model.KlassenLehrer;
 import ch.grademasters.model.Pruefung;
 import ch.grademasters.model.Zeugnis;
 
+/**
+ * @description Implementierte Methoden des Interfaces ZeugnisDao
+ * @author Luca Marti, Chiramet Phong Penglerd, Elia Perenzin
+ *         ZeugnisJDBCDao.java Copyright Berufsbildungscenter GradeMasters 2015
+ */
 public class ZeugnisJDBCDao extends Database implements ZeugnisDao {
+	// Connection auf null setzen
 	private Connection con = null;
 
+	/**
+	 * @description Zerstellt das Zeugnis
+	 * @param int klassen_ID
+	 * @throws SQLException
+	 * @return zeugnisse
+	 */
 	public ArrayList<Zeugnis> getZeugnis(int klassen_ID) throws SQLException {
 		ArrayList<Zeugnis> zeugnisse = new ArrayList<Zeugnis>();
 
+		// Query => List alle Faecher aus der Tabelle Fach, wo die Klassen_ID =
+		// ? ist
 		String sql = "Select * from FACH WHERE Klasse_ID = ?;";
+		// Holt die Connection
 		con = getCon();
 		ps = con.prepareStatement(sql);
+		// Klassen_ID erhalten
 		ps.setInt(1, klassen_ID);
+		// Query ausfuehren
 		rs = ps.executeQuery();
 
+		// Neues Zeugnis erstellen
 		Zeugnis zeugnis = new Zeugnis();
+		// Neue Klasse erstellen
 		Klasse klasse;
+		// Neuer Klassenlehrer erstellen
 		KlassenLehrer klassenLehrer;
+		// Query welcher die Klasse, Schule, Semerster und Klassenlehrer_ID von
+		// der Klasse herauslist, wo die ID_Klasse = ? ist.
 		String sql3 = "Select Klasse, Schule, Semester, Klassenlehrer_ID From Klasse WHERE ID_Klasse = ?;";
+		//Holt die Conection
 		con = getCon();
 		ps3 = con.prepareStatement(sql3);
+		//Bekommt die Klasse
 		ps3.setInt(1, klassen_ID);
+		//Query ausfuehren
 		rs3 = ps3.executeQuery();
+		//Solang es Werte hat, wiederhole das
 		while (rs3.next()) {
-
+			//Query welches den Name, Vorname und Email von der Tabelle Klassenlehrer list, wo die ID = ? ist
 			String sql4 = "Select Name, Vorname, Email From KLASSENLEHRER WHERE ID_Klassenlehrer = ?;";
 			con = getCon();
 			ps4 = con.prepareStatement(sql4);
+			//ERhaelt die Klassenlehrer_ID
 			ps4.setInt(1, rs3.getInt("Klassenlehrer_ID"));
+			//Query ausfuehren
 			rs4 = ps4.executeQuery();
+			//Solange es Werte hat, wiederhole das
 			while (rs4.next()) {
+				//Klassenlehrer erstellen und Werte hinzufuegen
 				klassenLehrer = new KlassenLehrer(rs4.getString("Name"),
 						rs4.getString("Vorname"), rs4.getString("Email"));
+				//Klasse erstellen und Werte hinzufuegen
 				klasse = new Klasse(klassenLehrer, rs3.getString("Klasse"),
 						rs3.getString("Schule"), rs3.getInt("Semester"));
+				//KlassenLehrer und klasse dem Zeugnis adden
 				zeugnis.setKlassenLehrer(klassenLehrer);
 				zeugnis.setKlasse(klasse);
 				break;
 			}
 		}
-
+		//Fach erstellen
 		Fach fach;
+		//Pruefung erstellen
 		Pruefung pruefung;
+		//Solange es Warte hat, wiederhole das
 		while (rs.next()) {
+			//Neues Fach erstellen
 			fach = new Fach(rs.getString("Fach"));
+			//Fach ID erhalten
 			int fach_ID = rs.getInt("ID_Fach");
+			//Fach dem Zeugnis adden
 			zeugnis.addFach(fach);
 
+			//Query welches alles aus der Tabelle Preufung liest, wo die ID = ?
 			String sql2 = "Select * from PRUEFUNG WHERE FACH_ID = ?;";
 			ps2 = con.prepareStatement(sql2);
+			//Fach_ID erhalten
 			ps2.setInt(1, fach_ID);
+			//Query ausfuehren
 			rs2 = ps2.executeQuery();
 
+			//Wiederholen solange Werte hat
 			while (rs2.next()) {
+				//Neue Pruefung erstellen und Werte hinzufuegen
 				pruefung = new Pruefung(null, rs2.getFloat("Note"),
 						rs2.getFloat("Gewichtung"), null);
+				//Preufung dem Fach hinzufuegen
 				fach.addPruefung(pruefung);
 			}
+			//Zeugnis dem Zeugnisse adden
 			zeugnisse.add(zeugnis);
 		}
+		//Connection closen
 		closeCon();
+		//Zeugnisse zurueck geben
 		return zeugnisse;
 	}
 
--- src/ch/grademasters/pdf/ZeugnisPdf.java
+++ src/ch/grademasters/pdf/ZeugnisPdf.java
@@ -27,6 +27,8 @@ import com.itextpdf.text.pdf.PdfWriter;
 
 public class ZeugnisPdf {
 	/* Schriften erstellen */
+	// SChriftart mit ihren verschiedenen Attributen: Schritart, Groesse und
+	// Style
 	private static Font helvetica1 = new Font(Font.FontFamily.HELVETICA, 22,
 			Font.BOLD);
 	private static Font helvetica2 = new Font(Font.FontFamily.HELVETICA, 16,
@@ -42,11 +44,10 @@ public class ZeugnisPdf {
 	static SimpleDateFormat aktuellesDatum = new SimpleDateFormat("dd.MM.yyyy");
 	// Aktuelle Zeit
 	static SimpleDateFormat aktuelleZeit = new SimpleDateFormat("HH:mm:ss");
-
+	// Variable Int fuer klassen_ID
 	private static int klasse_ID;
 
-	// Output Ordner
-
+	// Getter und Setter fuer klasse_ID
 	private static int getKlasse_ID() {
 		return klasse_ID;
 	}
@@ -55,10 +56,15 @@ public class ZeugnisPdf {
 		ZeugnisPdf.klasse_ID = klasse_ID;
 	}
 
+	/**
+	 * @descrption Erstellt das Zeugnis
+	 * @param klasse_ID
+	 */
 	public ZeugnisPdf(int klasse_ID) {
 		this.setKlasse_ID(klasse_ID);
 
 		try {
+			// boolean fuer die Variable exist
 			boolean exist = true;
 			// Neues Dokument anlegen
 			Document document = new Document();
@@ -68,7 +74,8 @@ public class ZeugnisPdf {
 				// Output Ordner + file Name
 				String file = "Zeugnisse/" + "Zeugnis" + i + "-"
 						+ aktuellesDatum.format(zeitStempel) + ".pdf";
-				// Wenn das File schon existiert, dann benenne es um
+				// Wenn das File schon existiert, dann benenne es um, indem der
+				// Zaehler um 1 erhoeht wird
 				if (new File(file).exists()) {
 					// Zaehler erhoehen
 					i = i + 1;
@@ -77,7 +84,7 @@ public class ZeugnisPdf {
 				else {
 					// Schreibe ein neues File
 					PdfWriter.getInstance(document, new FileOutputStream(file));
-					// exist auf false setzen, dass keine Unendlich SChlaufe
+					// exist auf false setzen, dass keine Unendlich Schlaufe
 					// produziert wird
 					exist = false;
 				}
@@ -87,21 +94,27 @@ public class ZeugnisPdf {
 			// Meta Tags adden
 			addMetaData(document);
 			// Page Adden
-			addTitlePage(document);
+			addContentPage(document);
 			// Dokument schliessen
 			document.close();
 
 			// Wenn alles gut geht, gibt es Okö aus.
 			// System.out.println("Okö");
 			i = i + 1;
-			// Exception behandeln
 		}
+		// Exception behandeln
 		catch (FileNotFoundException | DocumentException e) {
-			System.err.println("Kein Zugriff auf den Ordner." + "\n"
-					+ "Bitte überprüfen Sie den Pfad!" + "\n");
+			// Wenn das File oder der Ordner nicht gefunden werden konnte, dann
+			// gibt es folgenden Text aus:
+			System.err
+					.println("Kein Zugriff auf den Ordner"
+							+ "Grademasters/Zeugnisse/"
+							+ "\n"
+							+ "Bitte überprüfen Sie den Pfad, ob der Ordner vorhanden ist!"
+							+ "\n");
 			System.out
 					.println("Wenn das Problem weiter besteht, Melden Sie sich bitte beim Entwickler."
-							+ "\n" + "Error-Meldung:" + "\n");
+							+ "\n\n" + "**Error-Meldung:**" + "\n");
 			e.printStackTrace();
 		}
 		catch (Exception e) {
@@ -118,84 +131,114 @@ public class ZeugnisPdf {
 	//
 	// }
 
+	/**
+	 * @description Erstellt die Meta-Tags fuer das PDF File
+	 * @param document
+	 */
 	private static void addMetaData(Document document) {
 		document.addTitle("Zegnis Nr." + i);
 		document.addSubject("Created with iText by GradeMasters");
-		document.addKeywords("GradeMasters, Zeugnis, Schule, Note, Fach, Cooles Programm");
+		document.addKeywords("GradeMasters, Zeugnis, Schule, Note, Fach, Cooles Programm, Quizmasters.ch");
 		document.addAuthor("GradeMasters");
 		document.addCreator("GradeMasters");
 	}
 
-	private static void addTitlePage(Document document)
+	/**
+	 * @description erstellt das ganze File
+	 * @param document
+	 * @throws DocumentException
+	 */
+	private static void addContentPage(Document document)
 			throws DocumentException {
+		// Neuer Paragraph erstellen, welcher das ganze PDF ist
 		Paragraph ganzesPdfFile = new Paragraph();
 
+		// Pfad des Logos
 		String logo = "src/images/logo.png";
 		Image image = null;
+		// Versuchen das Logo zu laden
 		try {
-			image = Image.getInstance((logo));
+			image = Image.getInstance(logo);
 		}
+		// Exception behandeln
 		catch (MalformedURLException e) {
+			System.err.println("Das Logo konnte nicht geladen werden!");
+			System.out.println("\n\nFehler-Meldung:\n");
 			e.printStackTrace();
 		}
 		catch (IOException e) {
+			System.err.println("Das Logo konnte nicht geladen werden!");
+			System.out.println("\n\nFehler-Meldung:\n");
 			e.printStackTrace();
 		}
+		// Bildposition setzen
 		image.setAbsolutePosition(465f, 720f);
+		// Bild skalieren auf gewuenschte Groesse (90x90)
 		image.scaleAbsolute(90, 90);
 
+		// Zeugnis Liste
 		ArrayList<Zeugnis> zeugnis = GMController.getInstance().getZeugnis(
 				getKlasse_ID());
 
 		/* Layout */
+		// Leere Zeile(n) Anzahl(1) hinzufuegen
 		addEmptyLine(ganzesPdfFile, 1);
+		// Titel von dem PDF hinzufuegen
 		ganzesPdfFile.add(new Paragraph("Zeugnis Nr. " + i, helvetica1));
+		// Bild dem PDF hinzufuegen (Logo Gm)
 		ganzesPdfFile.add(image);
+		// Leere Zeile(n) Anzahl(2) hinzufuegen
 		addEmptyLine(ganzesPdfFile, 2);
+		// Ueberschrift fuer den Inhalt an das PDF adden
 		ganzesPdfFile.add(new Paragraph("Zeugnis erstellt von: "
 				+ System.getProperty("user.name") + ", "
 				+ aktuellesDatum.format(zeitStempel) + " - "
 				+ aktuelleZeit.format(zeitStempel), helvetica2));
+		// Leere Zeile(n) Anzahl(1) hinzufuegen
 		addEmptyLine(ganzesPdfFile, 1);
-		ganzesPdfFile.add(new Paragraph(
-				"Dein erstelltes Zeugnis ist nicht dein Endzeugnis. Hier fliessen keine Mündlichen Noten, etc. mit ein!",
-				helvetica3));
-		ganzesPdfFile.add(new Paragraph(
-				"_____________________________________________________________________________"));
+		// Hinweis hinzufuegen
+		ganzesPdfFile
+				.add(new Paragraph(
+						"Dein erstelltes Zeugnis ist nicht dein Endzeugnis. Hier fliessen keine Mündlichen Noten, etc. mit ein!",
+						helvetica3));
+		// Trennlinie hinzufuegen
+		ganzesPdfFile
+				.add(new Paragraph(
+						"_____________________________________________________________________________"));
+		// Leere Zeile(n) Anzahl(2) hinzufuegen
 		addEmptyLine(ganzesPdfFile, 2);
+		// TExt hinzufuegen
 		ganzesPdfFile.add(new Paragraph("Klassen-Informationen:"));
+		// Zeugnis Informationen herauslesen
 		for (Zeugnis zeugnisGross : zeugnis) {
 			ganzesPdfFile.add(zeugnisGross.toString());
 			break;
 		}
+		// Leere Zeile(n) Anzahl(3) hinzufuegen
 		addEmptyLine(ganzesPdfFile, 3);
-		ganzesPdfFile.add(new Paragraph("_____________________________________",
-				helvetica3));
-		ganzesPdfFile.add(new Paragraph("Herzliche Gratulation zu Deinem Zeugnis!"));
+		// Trennlinie hinzufuegen
+		ganzesPdfFile.add(new Paragraph(
+				"_____________________________________", helvetica3));
+		// Text hinzufuegen
+		ganzesPdfFile.add(new Paragraph(
+				"Herzliche Gratulation zu Deinem Zeugnis!"));
+		// Gruss Zeile hinzufuegen
 		ganzesPdfFile.add(new Paragraph("Freundliche Grüsse"));
 		ganzesPdfFile.add(new Paragraph("Dein GradeMasters Team"));
-
+		// ganzes PDF an dem Dokument hinzufuegen
 		document.add(ganzesPdfFile);
-		// Neue Seite
+		// Neue Seite erstellen
 		document.newPage();
 	}
 
+	/**
+	 * @description Leere Linie hinzufuegen
+	 * @param paragraph
+	 * @param number
+	 */
 	private static void addEmptyLine(Paragraph paragraph, int number) {
 		for (int i = 0; i < number; i++) {
 			paragraph.add(new Paragraph(" "));
 		}
 	}
-
-	public static Icon loadIcon(String iconName) {
-		final URL resource = GradeMastersView.class.getResource("/images/"
-				+ iconName);
-
-		if (resource == null) {
-			System.err.println("Error in " + GradeMastersView.class.getName()
-					+ ": Icon /images/" + iconName + " could not be loaded.");
-			return new ImageIcon();
-		}
-		return new ImageIcon(resource);
-	}
-
 }
