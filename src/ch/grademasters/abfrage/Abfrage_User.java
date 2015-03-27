e930716062ba51874c74e73c9deccfdd9d8ba5a1
zmartl <luca.marti@bbcag.ch>
2015-03-27 15:52:56 +0100

    Signed-off-by: zmartl <luca.marti@bbcag.ch>

--- src/ch/grademasters/abfrage/Abfrage_User.java
+++ src/ch/grademasters/abfrage/Abfrage_User.java
@@ -8,21 +8,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.IOException;
-import java.net.MalformedURLException;
 
-import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-import com.itextpdf.text.BadElementException;
-import com.itextpdf.text.Image;
-
 import ch.grademasters.controller.GMController;
 import ch.grademasters.exception.SQLError;
 import ch.grademasters.model.User;
-import ch.grademasters.pdf.ZeugnisPdf;
 import ch.grademasters.view.GradeMastersView;
 
 /**
--- src/ch/grademasters/view/GradeMastersView.java
+++ src/ch/grademasters/view/GradeMastersView.java
@@ -5,7 +5,6 @@ import java.awt.CardLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.Toolkit;
 import java.net.URL;
 import java.util.Calendar;
